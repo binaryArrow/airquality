06455441890098db74763df4c8067d65c8d4b96c
CCN <cancihan96@gmail.com>
2021-11-25 02:12:57 +0100

    config: added influxDB in backend

---
+++ backend/src/influxData.ts
@@ -0,0 +1,31 @@
+import {InfluxDB, Point, WriteApi} from '@influxdata/influxdb-client'
+
+export class InfluxConnection{
+    token: string
+    org: string
+    bucket: string
+    client: InfluxDB
+    writeApi: WriteApi
+    constructor() {
+        this.token = 'fQW5nbFGlYscP051Urvl1orhEOENA-one_r5zgVIbl_BSvian9kDUKlge00I5_lJ0M7rRJoFfLWXdsuRxEXXVA=='
+        this.org = 'AirqualityPT'
+        this.bucket = 'airquality'
+        this.client = new InfluxDB({url: 'http://localhost:8086', token: this.token})
+        this.writeApi = this.client.getWriteApi(this.org, this.bucket)
+        this.writeApi.useDefaultTags({host: 'host1'})
+    }
+
+    writePoint(point: Point){
+        this.writeApi.writePoint(point)
+        this.writeApi.close().then(()=>{
+            console.log('WRITE DONE')
+        })
+    }
+    writePoints(points: Point[]){
+        this.writeApi.writePoints(points)
+        this.writeApi.close().then(()=>{
+            console.log('WRITE DONE')
+        })
+    }
+
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,33 @@
 import express from 'express'
+import {InfluxConnection} from "./influxData";
+import {Point} from "@influxdata/influxdb-client";
 
 const app = express()
 
 app.get('/', (req, res) => {
+    const influxConnection: InfluxConnection = new InfluxConnection
+
+    let temperaturePoints = new Array<Point>()
+    let qualityPoints = new Array<Point>()
+
+    for(let i = 3; i <= 10; i++){
+        let temperaturePoint: Point = new Point('temperature')
+            .tag('sensor_id', 'S01')
+            .floatField('value', i)
+        temperaturePoints.push(temperaturePoint)
+
+        let qualityPoint: Point = new Point('quality')
+            .tag('sensor_id', 'S01')
+            .floatField('value', i + 20)
+        qualityPoints.push(qualityPoint)
+
+    }
+    influxConnection.writePoints(temperaturePoints)
+    influxConnection.writePoints(qualityPoints)
+
     res.send('Hello World');
+
 })
 
 app.listen(8000, () => console.log('Server running'))
+
