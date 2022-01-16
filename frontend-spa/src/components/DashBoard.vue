4fec0efa4b9f1221eb33e242bc7945830c983213
TheNameless31 <58726827+TheNameLess31@users.noreply.github.com>
2022-01-16 11:53:38 +0100

    d3 Library hinzugefügt, Dashboard Datei erstellt

---
+++ frontend-spa/src/components/DashBoard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="dash-board">
+    <svg :class="axes">
+      <line stroke-width="20px"/>
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import * as d3 from 'd3'
+
+export default {
+  name: "DashBoard",
+  data(){
+    return{
+      width: 1200,
+      height: 600,
+      margin: {
+        left: 20,
+        right: 10,
+        bottom: 10,
+        top: 20
+      },
+      scales:{
+        x: null,
+        y: null
+      }
+    }
+  }
+}
+
+
+</script>
+
+<style lang="scss">
+
+</style>
--- frontend-spa/src/views/About.vue
+++ frontend-spa/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hier kommt ein Koordinatensystem hin</h1>
+    <DashBoard />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import DashBoard from "@/components/DashBoard.vue";
+
+export default defineComponent({
+  name: 'About',
+  components: {
+        DashBoard,
+  }
+});
+</script>
