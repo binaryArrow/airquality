dac13655858ccf3b1847053fc71e834ea680fd28
Can Cihan Nazlier <cancihan96@gmail.com>
2022-02-09 22:58:09 +0100

    ref: temperature option hinzugefügt TODO: richtige werte aus der Datenbank ziehen

--- frontend-spa/src/components/DashBoard.vue
+++ frontend-spa/src/components/DashBoard.vue
@@ -7,9 +7,9 @@
         v-bind:chart-data3="tempData3"
         v-bind:x-axis="labels"
     />
-    <button class="button is-success" v-bind:disabled="waitButton" @click="sixHours">6 stunden</button>
-    <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay">1 Tag</button>
-    <button class="button is-success" v-bind:disabled="waitButton" @click="oneWeek">1 Woche</button>
+    <button class="button is-success" v-bind:disabled="waitButton" @click="sixHours('temperature')">6 stunden</button>
+    <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay('temperature')">1 Tag</button>
+    <button class="button is-success" v-bind:disabled="waitButton" @click="oneWeek('temperature')">1 Woche</button>
   </div>
 </template>
 
@@ -38,7 +38,6 @@ export default defineComponent({
           sensorData3: [],
           tempData3: [],
           waitButton: false,
-          time: 10080, // get data for 7 days
           communicator: new Communicator(),
           labels: []
         }
@@ -48,26 +47,32 @@ export default defineComponent({
         this.setXaxisHours(6)
       },
       methods: {
-        sixHours() {
+        sixHours(options) {
           this.waitButton = true
-          this.parseData(6 * 60, 6)
-          this.setXaxisHours(6)
+          if (options === "temperature") {
+            this.parseData(6 * 60, 6)
+            this.setXaxisHours(6)
+          }
           setTimeout(() => {
             this.waitButton = false
           }, 2000)
         },
-        oneDay() {
+        oneDay(options) {
           this.waitButton = true
-          this.parseData(24 * 60, 24)
-          this.setXaxisHours(24)
+          if (options === "temperature") {
+            this.parseData(24 * 60, 24)
+            this.setXaxisHours(24)
+          }
           setTimeout(() => {
             this.waitButton = false
           }, 2000)
         },
-        oneWeek() {
+        oneWeek(options) {
           this.waitButton = true
-          this.parseData(168 * 60, 168)
-          this.setXaxisHours(168)
+          if (options === "temperature") {
+            this.parseData(168 * 60, 168)
+            this.setXaxisHours(168)
+          }
           setTimeout(() => {
             this.waitButton = false
           }, 2000)
@@ -76,14 +81,14 @@ export default defineComponent({
           this.tempData1 = []
           this.tempData2 = []
           this.tempData3 = []
-          this.medianCalculation(3)
+          this.medianCalculationTemp(3)
         },
         calculate24Hours() {
           this.loaded = false
           this.tempData1 = []
           this.tempData2 = []
           this.tempData3 = []
-          this.medianCalculation(12)
+          this.medianCalculationTemp(12)
           this.loaded = true
         },
         calculate1Week() {
@@ -91,10 +96,10 @@ export default defineComponent({
           this.tempData1 = []
           this.tempData2 = []
           this.tempData3 = []
-          this.medianCalculation(84)
+          this.medianCalculationTemp(84)
           this.loaded = true
         },
-        medianCalculation(medianSize) {
+        medianCalculationTemp(medianSize) {
           const medianCalculationSize = medianSize
           let median = 0
           for (let i = 0; i < this.sensorData1.length; i++) {
@@ -124,22 +129,22 @@ export default defineComponent({
             }
           }
         },
-        async parseData(hours, options) {
+        async parseData(dataSet, options) {
           this.loaded = false
           this.sensorData1 = []
-          await this.communicator.getSensorData(1, hours).then(data => {
+          await this.communicator.getSensorData(1, dataSet).then(data => {
             data.forEach(value => {
               this.sensorData1.push(value)
             })
           })
           this.sensorData2 = []
-          await this.communicator.getSensorData(2, hours).then(data => {
+          await this.communicator.getSensorData(2, dataSet).then(data => {
             data.forEach(value => {
               this.sensorData2.push(value)
             })
           })
           this.sensorData3 = []
-          await this.communicator.getSensorData(3, hours).then(data => {
+          await this.communicator.getSensorData(3, dataSet).then(data => {
             data.forEach(value => {
               this.sensorData3.push(value)
             })
@@ -173,20 +178,20 @@ export default defineComponent({
             }
             case 24: {
               let startDate = new Date(moment.now());
-              startDate.setHours(startDate.getHours()-24)
+              startDate.setHours(startDate.getHours() - 24)
               for (let i = 0; i < 120; i++) {
                 const date = moment(startDate).add(12, 'minutes').format('HH:mm');
-                startDate.setMinutes(startDate.getMinutes()+12)
+                startDate.setMinutes(startDate.getMinutes() + 12)
                 this.labels.push(date.toString());
               }
               break;
             }
             case 168: {
               let startDate = new Date(moment.now());
-              startDate.setHours(startDate.getHours()-168)
+              startDate.setHours(startDate.getHours() - 168)
               for (let i = 0; i < 120; i++) {
                 const date = moment(startDate).add(84, 'minutes').format('dd:HH:mm');
-                startDate.setMinutes(startDate.getMinutes()+84)
+                startDate.setMinutes(startDate.getMinutes() + 84)
                 this.labels.push(date.toString());
               }
               break;
