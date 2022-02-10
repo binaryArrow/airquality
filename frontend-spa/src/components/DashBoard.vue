e6e87b9d0b20f107a6aabfc02ac56ed9e638c8b7
Can Cihan Nazlier <cancihan96@gmail.com>
2022-02-10 16:35:52 +0100

    fixes

--- frontend-spa/src/components/DashBoard.vue
+++ frontend-spa/src/components/DashBoard.vue
@@ -5,7 +5,7 @@
         v-bind:chart-data1="tempData1"
         v-bind:chart-data2="tempData2"
         v-bind:chart-data3="tempData3"
-        v-bind:x-axis="labels"
+        v-bind:x-axis="labelsTemp"
     />
     <button class="button is-success" v-bind:disabled="waitButton" @click="sixHours('temperature')">6 stunden</button>
     <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay('temperature')">1 Tag</button>
@@ -17,7 +17,7 @@
         v-bind:chart-data1="humData1"
         v-bind:chart-data2="humData2"
         v-bind:chart-data3="humData3"
-        v-bind:x-axis="labels"
+        v-bind:x-axis="labelsHum"
     />
     <button class="button is-success" v-bind:disabled="waitButton" @click="sixHours('humidity')">6 stunden</button>
     <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay('humidity')">1 Tag</button>
@@ -29,7 +29,7 @@
         v-bind:chart-data1="co2Data1"
         v-bind:chart-data2="co2Data2"
         v-bind:chart-data3="co2Data3"
-        v-bind:x-axis="labels"
+        v-bind:x-axis="labelsCo2"
     />
     <button class="button is-success" v-bind:disabled="waitButton" @click="sixHours('co2')">6 stunden</button>
     <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay('co2')">1 Tag</button>
@@ -62,7 +62,7 @@ export default defineComponent({
           sensorData1: [],
           tempData1: [],
           humData1: [],
-          co2Data1:[],
+          co2Data1: [],
           sensorData2: [],
           tempData2: [],
           humData2: [],
@@ -70,28 +70,32 @@ export default defineComponent({
           sensorData3: [],
           tempData3: [],
           humData3: [],
-          co2Data3:[],
+          co2Data3: [],
           waitButton: false,
           communicator: new Communicator(),
-          labels: []
+          labelsTemp: [],
+          labelsHum: [],
+          labelsCo2: []
         }
       },
       created() {
         this.parseData(6 * 60, 0)
-        this.setXaxisHours(6)
+        this.setXaxisHours(6, "temperature")
+        this.setXaxisHours(6, "co2")
+        this.setXaxisHours(6, "humidity")
       },
       methods: {
         sixHours(options) {
           this.waitButton = true
           if (options === "temperature") {
             this.parseData(6 * 60, 6, options)
-            this.setXaxisHours(6)
-          } else if(options === "humidity"){
+            this.setXaxisHours(6, options)
+          } else if (options === "humidity") {
             this.parseData(6 * 60, 6, options)
-            this.setXaxisHours(6)
-          }else if (options === "co2"){
+            this.setXaxisHours(6, options)
+          } else if (options === "co2") {
             this.parseData(6 * 60, 6, options)
-            this.setXaxisHours(6)
+            this.setXaxisHours(6, options)
           }
           setTimeout(() => {
             this.waitButton = false
@@ -101,13 +105,13 @@ export default defineComponent({
           this.waitButton = true
           if (options === "temperature") {
             this.parseData(24 * 60, 24, options)
-            this.setXaxisHours(24)
-          }else if(options === "humidity"){
+            this.setXaxisHours(24, options)
+          } else if (options === "humidity") {
             this.parseData(24 * 60, 24, options)
-            this.setXaxisHours(24)
-          }else if (options === "co2"){
-            this.parseData(6 * 60, 6, options)
-            this.setXaxisHours(6)
+            this.setXaxisHours(24, options)
+          } else if (options === "co2") {
+            this.parseData(24 * 60, 6, options)
+            this.setXaxisHours(24, options)
           }
           setTimeout(() => {
             this.waitButton = false
@@ -117,52 +121,54 @@ export default defineComponent({
           this.waitButton = true
           if (options === "temperature") {
             this.parseData(168 * 60, 168, options)
-            this.setXaxisHours(168)
-          }else if(options === "humidity"){
+            this.setXaxisHours(168, options)
+          } else if (options === "humidity") {
             this.parseData(168 * 60, 168, options)
-            this.setXaxisHours(168)
-          }else if (options === "co2"){
-            this.parseData(6 * 60, 6, options)
-            this.setXaxisHours(6)
+            this.setXaxisHours(168, options)
+          } else if (options === "co2") {
+            this.parseData(168 * 60, 6, options)
+            this.setXaxisHours(168, options)
           }
           setTimeout(() => {
             this.waitButton = false
           }, 2000)
         },
         calculate6Hours(options) {
-          if(options === "temperature"){
+          if (options === "temperature") {
             this.tempData1 = []
             this.tempData2 = []
             this.tempData3 = []
             this.medianCalculation(3, options)
-          }else if(options === "humidity"){
+          } else if (options === "humidity") {
             this.humData1 = []
             this.humData2 = []
             this.humData3 = []
             this.medianCalculation(3, options)
-          }else if(options === "co2"){
+          } else if (options === "co2") {
             this.co2Data1 = []
             this.co2Data2 = []
             this.co2Data3 = []
-            this.medianCalculationCO2(3, options)}
+            this.medianCalculation(3, options)
+          }
         },
         calculate24Hours(options) {
           this.loaded = false
-          if(options === "temperature"){
+          if (options === "temperature") {
             this.tempData1 = []
             this.tempData2 = []
             this.tempData3 = []
             this.medianCalculation(12, options)
-          }else if(options === "humidity"){
+          } else if (options === "humidity") {
             this.humData1 = []
             this.humData2 = []
             this.humData3 = []
             this.medianCalculation(12, options)
-          }else if(options === "co2"){
+          } else if (options === "co2") {
             this.co2Data1 = []
             this.co2Data2 = []
             this.co2Data3 = []
-            this.medianCalculationCO2(12)}
+            this.medianCalculation(12, options)
+          }
           this.loaded = true
         },
         calculate1Week(options) {
@@ -178,16 +184,17 @@ export default defineComponent({
             this.humData3 = []
             this.medianCalculation(84, options)
             this.loaded = true
-          }else if(options === "co2"){
+          } else if (options === "co2") {
             this.co2Data1 = []
             this.co2Data2 = []
             this.co2Data3 = []
-            this.medianCalculationCO2(84)}
+            this.medianCalculation(84, options)
+          }
         },
         medianCalculation(medianSize, options) {
           const medianCalculationSize = medianSize
           let median = 0
-          if(options === "temperature"){
+          if (options === "temperature") {
             for (let i = 0; i < this.sensorData1.length; i++) {
               if (i % medianCalculationSize !== 0 && i !== 0) {
                 median += parseFloat(this.sensorData1[i - 1].tempSHT21) / 100
@@ -215,34 +222,33 @@ export default defineComponent({
               }
             }
           }
-        },
-        medianCalculationCO2(medianSize) {
-          const medianCalculationSize = medianSize
-          let median = 0
-          for (let i = 0; i < this.sensorData1.length; i++) {
-            if (i % medianCalculationSize !== 0 && i !== 0) {
-              median += parseFloat(this.sensorData1[i - 1].co2SCD41) / 100
-            } else if (i !== 0) {
-              this.co2Data1.push(median / medianCalculationSize)
-              median = 0
+          if (options === "co2") {
+            median = 0
+            for (let i = 0; i < this.sensorData1.length; i++) {
+              if (i % medianCalculationSize !== 0 && i !== 0) {
+                median += parseFloat(this.sensorData1[i - 1].co2SCD41) / 100
+              } else if (i !== 0) {
+                this.co2Data1.push(median / medianCalculationSize)
+                median = 0
+              }
             }
-          }
-          median = 0
-          for (let i = 0; i < this.sensorData2.length; i++) {
-            if (i % medianCalculationSize !== 0 && i !== 0) {
-              median += parseFloat(this.sensorData2[i - 1].co2SCD41) / 100
-            } else if (i !== 0) {
-              this.co2Data2.push(median / medianCalculationSize)
-              median = 0
+            median = 0
+            for (let i = 0; i < this.sensorData2.length; i++) {
+              if (i % medianCalculationSize !== 0 && i !== 0) {
+                median += parseFloat(this.sensorData2[i - 1].co2SCD41) / 100
+              } else if (i !== 0) {
+                this.co2Data2.push(median / medianCalculationSize)
+                median = 0
+              }
             }
-          }
-          median = 0
-          for (let i = 0; i < this.sensorData3.length; i++) {
-            if (i % medianCalculationSize !== 0 && i !== 0) {
-              median += parseFloat(this.sensorData3[i - 1].co2SCD41) / 100
-            } else if (i !== 0) {
-              this.co2Data3.push(median / medianCalculationSize)
-              median = 0
+            median = 0
+            for (let i = 0; i < this.sensorData3.length; i++) {
+              if (i % medianCalculationSize !== 0 && i !== 0) {
+                median += parseFloat(this.sensorData3[i - 1].co2SCD41) / 100
+              } else if (i !== 0) {
+                this.co2Data3.push(median / medianCalculationSize)
+                median = 0
+              }
             }
           }
         },
@@ -267,7 +273,12 @@ export default defineComponent({
             })
           })
           switch (options) {
-            case 0:
+            case 0: {
+              this.calculate6Hours("temperature")
+              this.calculate6Hours("co2")
+              this.calculate6Hours("humidity")
+              break
+            }
             case 6:
               this.calculate6Hours(optionsDecide)
               break;
@@ -280,38 +291,110 @@ export default defineComponent({
           }
           this.loaded = true
         },
-        setXaxisHours(hours) {
-          this.labels = []
-          switch (hours) {
-            case 6: {
-              const startDate = new Date(moment.now());
-              startDate.setHours(startDate.getHours() - 6)
-              for (let i = 0; i < 120; i++) {
-                const date = moment(startDate).add(3, 'minutes').format('HH:mm');
-                startDate.setMinutes(startDate.getMinutes() + 3)
-                this.labels.push(date.toString());
+        setXaxisHours(hours, options) {
+          if (options === "temperature") {
+            this.labelsTemp = []
+            switch (hours) {
+              case 6: {
+                const startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 6)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(3, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 3)
+                  this.labelsTemp.push(date.toString());
+                }
+                break;
               }
-              break;
+              case 24: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 24)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(12, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 12)
+                  this.labelsTemp.push(date.toString());
+                }
+                break;
+              }
+              case 168: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 168)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(84, 'minutes').format('dd:HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 84)
+                  this.labelsTemp.push(date.toString());
+                }
+                break;
+              }
             }
-            case 24: {
-              let startDate = new Date(moment.now());
-              startDate.setHours(startDate.getHours() - 24)
-              for (let i = 0; i < 120; i++) {
-                const date = moment(startDate).add(12, 'minutes').format('HH:mm');
-                startDate.setMinutes(startDate.getMinutes() + 12)
-                this.labels.push(date.toString());
+          }
+          else if (options === "co2") {
+            this.labelsCo2 = []
+            switch (hours) {
+              case 6: {
+                const startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 6)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(3, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 3)
+                  this.labelsCo2.push(date.toString());
+                }
+                break;
               }
-              break;
+              case 24: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 24)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(12, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 12)
+                  this.labelsCo2.push(date.toString());
+                }
+                break;
+              }
+              case 168: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 168)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(84, 'minutes').format('dd:HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 84)
+                  this.labelsCo2.push(date.toString());
+                }
+                break;
+              }
             }
-            case 168: {
-              let startDate = new Date(moment.now());
-              startDate.setHours(startDate.getHours() - 168)
-              for (let i = 0; i < 120; i++) {
-                const date = moment(startDate).add(84, 'minutes').format('dd:HH:mm');
-                startDate.setMinutes(startDate.getMinutes() + 84)
-                this.labels.push(date.toString());
+          }
+          else if (options === "humidity") {
+            this.labelsHum = []
+            switch (hours) {
+              case 6: {
+                const startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 6)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(3, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 3)
+                  this.labelsHum.push(date.toString());
+                }
+                break;
               }
-              break;
+              case 24: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 24)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(12, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 12)
+                  this.labelsHum.push(date.toString());
+                }
+                break;
+              }
+              case 168: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 168)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(84, 'minutes').format('dd:HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 84)
+                  this.labelsHum.push(date.toString());
+                }
+                break;
+              }
             }
           }
         }
@@ -326,6 +409,7 @@ export default defineComponent({
   height: 250px;
   width: 800px;
 }
+
 #hum-div {
   height: 250px;
   width: 800px;
@@ -333,6 +417,7 @@ export default defineComponent({
   left: 850px;
   bottom: 250px;
 }
+
 #co2-div {
   height: 250px;
   width: 800px;
