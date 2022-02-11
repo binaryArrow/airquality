dc4ed4b97391fa33e8ad6145da09f64e7a67df61
stibbo55 <stipek55@gmail.com>
2022-02-11 22:17:54 +0100

    Add: humidity data added to hum graph & created tvoc graph and added tvoc data to it.

--- frontend-spa/src/components/DashBoard.vue
+++ frontend-spa/src/components/DashBoard.vue
@@ -35,6 +35,18 @@
     <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay('co2')">1 Tag</button>
     <button class="button is-success" v-bind:disabled="waitButton" @click="oneWeek('co2')">1 Woche</button>
   </div>
+  <div id="tvoc-div">
+    <tvoc-chart
+        v-if="loaded"
+        v-bind:chart-data1="tvocData1"
+        v-bind:chart-data2="tvocData2"
+        v-bind:chart-data3="tvocData3"
+        v-bind:x-axis="labelsTvoc"
+    />
+    <button class="button is-success" v-bind:disabled="waitButton" @click="sixHours('tvoc')">6 Stunden</button>
+    <button class="button is-success" v-bind:disabled="waitButton" @click="oneDay('tvoc')">1 Tag</button>
+    <button class="button is-success" v-bind:disabled="waitButton" @click="oneWeek('tvoc')">1 Woche</button>
+  </div>
 </template>
 
 <script>
@@ -43,6 +55,7 @@ import {Bar} from "vue3-chart-v2";
 import TemperatureChart from "@/components/TemperatureChart.vue";
 import Co2Chart from "@/components/Co2Chart.vue";
 import HumidityChart from "@/components/HumidityChart";
+import TvocChart from "@/components/TvocChart";
 import {Communicator} from "@/service/communicator";
 import moment from "moment";
 
@@ -53,7 +66,8 @@ export default defineComponent({
       components: {
         TemperatureChart,
         HumidityChart,
-        Co2Chart
+        Co2Chart,
+        TvocChart
       },
       data() {
         return {
@@ -63,19 +77,23 @@ export default defineComponent({
           tempData1: [],
           humData1: [],
           co2Data1: [],
+          tvocData1: [],
           sensorData2: [],
           tempData2: [],
           humData2: [],
           co2Data2: [],
+          tvocData2: [],
           sensorData3: [],
           tempData3: [],
           humData3: [],
           co2Data3: [],
+          tvocData3: [],
           waitButton: false,
           communicator: new Communicator(),
           labelsTemp: [],
           labelsHum: [],
-          labelsCo2: []
+          labelsCo2: [],
+          labelsTvoc: []
         }
       },
       created() {
@@ -83,6 +101,7 @@ export default defineComponent({
         this.setXaxisHours(6, "temperature")
         this.setXaxisHours(6, "co2")
         this.setXaxisHours(6, "humidity")
+        this.setXaxisHours(6, "tvoc")
       },
       methods: {
         sixHours(options) {
@@ -94,6 +113,9 @@ export default defineComponent({
             this.parseData(6 * 60, 6, options)
             this.setXaxisHours(6, options)
           } else if (options === "co2") {
+            this.parseData(6 * 60, 6, options)
+            this.setXaxisHours(6, options)
+          }else if (options === "tvoc") {
             this.parseData(6 * 60, 6, options)
             this.setXaxisHours(6, options)
           }
@@ -112,6 +134,9 @@ export default defineComponent({
           } else if (options === "co2") {
             this.parseData(24 * 60, 24, options)
             this.setXaxisHours(24, options)
+          }else if (options === "tvoc") {
+            this.parseData(24 * 60, 24, options)
+            this.setXaxisHours(24, options)
           }
           setTimeout(() => {
             this.waitButton = false
@@ -126,6 +151,9 @@ export default defineComponent({
             this.parseData(168 * 60, 168, options)
             this.setXaxisHours(168, options)
           } else if (options === "co2") {
+            this.parseData(168 * 60, 168, options)
+            this.setXaxisHours(168, options)
+          }else if (options === "tvoc") {
             this.parseData(168 * 60, 168, options)
             this.setXaxisHours(168, options)
           }
@@ -149,6 +177,11 @@ export default defineComponent({
             this.co2Data2 = []
             this.co2Data3 = []
             this.axisCalculation(3, options)
+          }else if (options === "tvoc") {
+            this.tvocData1 = []
+            this.tvocData2 = []
+            this.tvocData3 = []
+            this.axisCalculation(3, options)
           }
         },
         calculate24Hours(options) {
@@ -167,6 +200,11 @@ export default defineComponent({
             this.co2Data1 = []
             this.co2Data2 = []
             this.co2Data3 = []
+            this.axisCalculation(12, options)
+          }else if (options === "tvoc") {
+            this.tvocData1 = []
+            this.tvocData2 = []
+            this.tvocData3 = []
             this.axisCalculation(12, options)
           }
           this.loaded = true
@@ -189,11 +227,17 @@ export default defineComponent({
             this.co2Data2 = []
             this.co2Data3 = []
             this.axisCalculation(84, options)
+          }else if (options === "tvoc") {
+            this.tvocData1 = []
+            this.tvocData2 = []
+            this.tvocData3 = []
+            this.axisCalculation(84, options)
           }
         },
         axisCalculation(medianSize, options) {
           const medianCalculationSize = medianSize
           const nowDate = moment(moment.now())
+          // ##### TEMPERATURE #####
           if (options === "temperature") {
             let finalSensorData1 = []
             let finalSensorData2 = []
@@ -294,6 +338,109 @@ export default defineComponent({
               }
             })
           }
+          // ##### HUMIDITY #####
+          else if (options === "humidity") {
+            let finalSensorData1 = []
+            let finalSensorData2 = []
+            let finalSensorData3 = []
+            for (let i = 0; i < 120; i++) {
+              this.humData1.push(null)
+            }
+            for (let i = 0; i < 120; i++) {
+              this.humData2.push(null)
+            }
+            for (let i = 0; i < 120; i++) {
+              this.humData3.push(null)
+            }
+            for (let i = 0; i < this.sensorData1.length; i++) {
+              if (i % medianCalculationSize === 0 && i !== 0) {
+                if (this.sensorData1[i])
+                  finalSensorData1.push({
+                    data: parseFloat(this.sensorData1[i].humSCD41) / 100,
+                    timestamp: this.sensorData1[i].created_at
+                  })
+              }
+            }
+            for (let i = 0; i < this.sensorData2.length; i++) {
+              if (i % medianCalculationSize === 0 && i !== 0) {
+                if (this.sensorData2[i])
+                  finalSensorData2.push({
+                    data: parseFloat(this.sensorData2[i].humSCD41) / 100,
+                    timestamp: this.sensorData2[i].created_at
+                  })
+              }
+            }
+            for (let i = 0; i < this.sensorData3.length; i++) {
+              if (i % medianCalculationSize === 0 && i !== 0) {
+                if (this.sensorData3[i])
+                  finalSensorData3.push({
+                    data: parseFloat(this.sensorData3[i].humSCD41) / 100,
+                    timestamp: this.sensorData3[i].created_at
+                  })
+              }
+            }
+
+            finalSensorData1.forEach(data=>{
+              if(nowDate.isAfter(data.timestamp)){
+                let diff = moment(data.timestamp,'YYYY-MM-DD HH:mm:ss').diff(nowDate, 'minutes')
+                let diffTicks = Math.floor(Math.abs(diff/medianSize))
+                this.humData1[this.humData1.length-diffTicks] = data.data
+                console.log(medianSize)
+                switch (medianSize){
+                  case 12:
+                  case 3: {
+                    if(diffTicks === 0)
+                      diffTicks = 1
+                    this.labelsHum[this.humData1.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
+                    break;
+                  }
+                  case 84: {
+                    if(diffTicks === 0)
+                      diffTicks = 1
+                    this.labelsHum[this.humData1.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('DD dd:HH:mm')
+                    break;
+                  }
+                }
+              }
+            })
+            finalSensorData2.forEach(data=>{
+              if(nowDate.isAfter(data.timestamp)){
+                let diff = moment(data.timestamp,'YYYY-MM-DD HH:mm:ss').diff(nowDate, 'minutes')
+                let diffTicks = Math.floor(Math.abs(diff/medianSize))
+                this.humData2[this.humData2.length-diffTicks] = data.data
+                switch (medianSize){
+                  case 12:
+                  case 3: {
+                    this.labelsHum[this.humData2.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
+                    break;
+                  }
+                  case 84: {
+                    this.labelsHum[this.humData2.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('DD dd:HH:mm')
+                    break;
+                  }
+                }
+              }
+            })
+            finalSensorData3.forEach(data=>{
+              if(nowDate.isAfter(data.timestamp)){
+                let diff = moment(data.timestamp,'YYYY-MM-DD HH:mm:ss').diff(nowDate, 'minutes')
+                let diffTicks = Math.floor(Math.abs(diff/medianSize))
+                this.humData3[this.humData3.length-diffTicks] = data.data
+                switch (medianSize){
+                  case 12:
+                  case 3: {
+                    this.labelsHum[this.humData3.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
+                    break;
+                  }
+                  case 84: {
+                    this.labelsHum[this.humData3.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('DD dd:HH:mm')
+                    break;
+                  }
+                }
+              }
+            })
+          }
+          // ##### CO2 #####
           else if (options === "co2") {
             let finalSensorData1 = []
             let finalSensorData2 = []
@@ -394,6 +541,107 @@ export default defineComponent({
                 }
               }
             })
+          }// ##### TVOC #####
+          else if (options === "tvoc") {
+            let finalSensorData1 = []
+            let finalSensorData2 = []
+            let finalSensorData3 = []
+            for (let i = 0; i < 120; i++) {
+              this.tvocData1.push(null)
+            }
+            for (let i = 0; i < 120; i++) {
+              this.tvocData2.push(null)
+            }
+            for (let i = 0; i < 120; i++) {
+              this.tvocData3.push(null)
+            }
+            for (let i = 0; i < this.sensorData1.length; i++) {
+              if (i % medianCalculationSize === 0 && i !== 0) {
+                if (this.sensorData1[i])
+                  finalSensorData1.push({
+                    data: parseFloat(this.sensorData1[i].tvocCCS811) / 100,
+                    timestamp: this.sensorData1[i].created_at
+                  })
+              }
+            }
+            for (let i = 0; i < this.sensorData2.length; i++) {
+              if (i % medianCalculationSize === 0 && i !== 0) {
+                if (this.sensorData2[i])
+                  finalSensorData2.push({
+                    data: parseFloat(this.sensorData2[i].tvocCCS811) / 100,
+                    timestamp: this.sensorData2[i].created_at
+                  })
+              }
+            }
+            for (let i = 0; i < this.sensorData3.length; i++) {
+              if (i % medianCalculationSize === 0 && i !== 0) {
+                if (this.sensorData3[i])
+                  finalSensorData3.push({
+                    data: parseFloat(this.sensorData3[i].tvocCCS811) / 100,
+                    timestamp: this.sensorData3[i].created_at
+                  })
+              }
+            }
+
+            finalSensorData1.forEach(data=>{
+              if(nowDate.isAfter(data.timestamp)){
+                let diff = moment(data.timestamp,'YYYY-MM-DD HH:mm:ss').diff(nowDate, 'minutes')
+                let diffTicks = Math.floor(Math.abs(diff/medianSize))
+                this.tvocData1[this.tvocData1.length-diffTicks] = data.data
+                console.log(medianSize)
+                switch (medianSize){
+                  case 12:
+                  case 3: {
+                    if(diffTicks === 0)
+                      diffTicks = 1
+                    this.labelsTvoc[this.tvocData1.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
+                    break;
+                  }
+                  case 84: {
+                    if(diffTicks === 0)
+                      diffTicks = 1
+                    this.labelsTvoc[this.tvocData1.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('DD dd:HH:mm')
+                    break;
+                  }
+                }
+              }
+            })
+            finalSensorData2.forEach(data=>{
+              if(nowDate.isAfter(data.timestamp)){
+                let diff = moment(data.timestamp,'YYYY-MM-DD HH:mm:ss').diff(nowDate, 'minutes')
+                let diffTicks = Math.floor(Math.abs(diff/medianSize))
+                this.tvocData2[this.tvocData2.length-diffTicks] = data.data
+                switch (medianSize){
+                  case 12:
+                  case 3: {
+                    this.labelsTvoc[this.tvocData2.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
+                    break;
+                  }
+                  case 84: {
+                    this.labelsTvoc[this.tvocData2.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('DD dd:HH:mm')
+                    break;
+                  }
+                }
+              }
+            })
+            finalSensorData3.forEach(data=>{
+              if(nowDate.isAfter(data.timestamp)){
+                let diff = moment(data.timestamp,'YYYY-MM-DD HH:mm:ss').diff(nowDate, 'minutes')
+                let diffTicks = Math.floor(Math.abs(diff/medianSize))
+                this.tvocData3[this.tvocData3.length-diffTicks] = data.data
+                switch (medianSize){
+                  case 12:
+                  case 3: {
+                    this.labelsTvoc[this.tvocData3.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('HH:mm')
+                    break;
+                  }
+                  case 84: {
+                    this.labelsTvoc[this.tvocData3.length - diffTicks] = moment(data.timestamp, 'YYYY-MM-DD HH:mm:ss').format('DD dd:HH:mm')
+                    break;
+                  }
+                }
+              }
+            })
           }
         },
         async parseData(dataSet, options, optionsDecide) {
@@ -421,6 +669,7 @@ export default defineComponent({
               this.calculate6Hours("temperature")
               this.calculate6Hours("co2")
               this.calculate6Hours("humidity")
+              this.calculate6Hours("tvoc")
               break
             }
             case 6:
@@ -538,6 +787,40 @@ export default defineComponent({
                 break;
               }
             }
+          }else if (options === "tvoc") {
+            this.labelsTvoc = []
+            switch (hours) {
+              case 6: {
+                const startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 6)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(3, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 3)
+                  this.labelsTvoc.push(date.toString());
+                }
+                break;
+              }
+              case 24: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 24)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(12, 'minutes').format('HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 12)
+                  this.labelsTvoc.push(date.toString());
+                }
+                break;
+              }
+              case 168: {
+                let startDate = new Date(moment.now());
+                startDate.setHours(startDate.getHours() - 168)
+                for (let i = 0; i < 120; i++) {
+                  const date = moment(startDate).add(84, 'minutes').format('dd:HH:mm');
+                  startDate.setMinutes(startDate.getMinutes() + 84)
+                  this.labelsTvoc.push(date.toString());
+                }
+                break;
+              }
+            }
           }
         }
       }
@@ -564,6 +847,14 @@ export default defineComponent({
   height: 250px;
   width: 800px;
   position: relative;
+}
+
+#tvoc-div {
+  height: 250px;
+  width: 800px;
+  position: relative;
+  left: 850px;
+  bottom: 250px;
 }
 
 </style>
